93605e65b897315c9553c37d697c85d1313d26fe
Ibrahim Khalilov <ibrahimkhalilov@Ibrahims-Mac-mini.local>
2021-05-19 17:00:49 -0400

    finish project in TypeScript

--- src/Cart/Cart.tsx
+++ src/Cart/Cart.tsx
@@ -9,17 +9,22 @@ type Props = {
 };
 
 const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart }) => {
+
+const calculateTotal = (items: CartItemType[]) =>
+items.reduce((ack: number, item) => ack + item.amount * item.price, 0);
+
   return (
     <Wrapper>
       <h2>Your Shopping Cart</h2>
       {cartItems.length === 0 ? <p>No items in Cart.</p> : null}
-      {cartItems.map((item) => (
+      {cartItems.map(item => ( 
         <CartItem
         key={item.id}
         item={item}
         addToCart={addToCart}
         removeFromCart={removeFromCart} />
       ))}
+      <h2>Total: ${calculateTotal(cartItems).toFixed(2)}</h2>
     </Wrapper>
   );
 };
--- src/CartItem/CartItem.tsx
+++ src/CartItem/CartItem.tsx
@@ -10,12 +10,13 @@ type Props = {
 
 const CartItem: React.FC<Props> = ({ item, addToCart, removeFromCart }) => (
   <Wrapper>
+    <div>
     <h3>{item.title}</h3>
     <div className='information'>
     <p>Price: ${item.price}</p>
     <p>Total: ${(item.amount * item.price).toFixed(2)}</p>
     </div>
-    <div>
+    <div className='buttons'>
       <Button size='small' disableElevation variant='contained' onClick={() => removeFromCart(item.id)}>
         -
       </Button>
@@ -24,8 +25,9 @@ const CartItem: React.FC<Props> = ({ item, addToCart, removeFromCart }) => (
         +
       </Button>
     </div>
+    </div>
     <img src={item.image} alt={item.title} />
   </Wrapper>
-)
+);
 
 export default CartItem;
